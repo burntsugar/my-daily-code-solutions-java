1de058777f3aba094afe9a83b8a820154af7a050
burntsugar <rrakky89@gmail.com>
2019-11-23 17:22:23 +1100

    Initial commit

---
+++ src/com/rach/Main.java
@@ -0,0 +1,22 @@
+package com.rach;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        problemOne();
+    }
+
+    public static void problemOne(){
+        int k = 17;
+        int[] arr = {10,15,3,7,2};
+        List<int[]> pairs = ProblemOne.solve(k,arr);
+        System.out.println("Number of pairs: " + pairs.size());
+        System.out.println("Pairs...");
+        for(int i = 0; i < pairs.size(); i++){
+            System.out.println(pairs.get(i)[0] + ", " + pairs.get(i)[1]);
+        }
+    }
+
+
+}
---
+++ src/com/rach/ProblemOne.java
@@ -0,0 +1,27 @@
+package com.rach;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProblemOne {
+
+    public static List<int[]> solve(int k, int[] arr){
+        List<int[]> pairs = new ArrayList<>();
+        Arrays.sort(arr);
+        int l = 0;
+        int r = arr.length -1;
+        while (l < r){
+            if(arr[l] + arr[r] == k) {
+                int[] a = {arr[l],arr[r]};
+                pairs.add(a);
+            }
+            if (arr[l] + arr[r] < k) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return pairs;
+    }
+}

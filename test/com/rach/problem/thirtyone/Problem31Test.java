25cb0e62b7d67a428d11a0b5965eec301851e639
burntsugar <rrakky89@gmail.com>
2019-12-22 12:02:55 +1100

    fix

--- test/com/rach/problem/thirtyone/Problem31Test.java
+++ test/com/rach/problem/thirtyone/Problem31Test.java
@@ -9,42 +9,42 @@ public class Problem31Test {
 
     @Nested
     @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-    class Edit_distance_is_calculated {
+    class Edit_Distance_Is_Calculated {
 
         @Test
-        @DisplayName("Edit distance between 'sitting' and 'kitten' is 3")
+        @DisplayName("Edit distance between 'sitting' and 'kitten'")
         void solve1() {
             String oldString = "sitting";
             String newString = "kitten";
             int expected = 3;
-            assertEquals(expected, Problem31.solve(oldString, newString));
+            assertEquals(expected, Problem31.solve(oldString, newString), () -> "expected should be 3");
         }
 
         @Test
-        @DisplayName("Edit distance between 'climate' and 'Climate Change' is 8")
+        @DisplayName("Edit distance between 'climate' and 'Climate Change'")
         void solve2() {
             String oldString = "climate";
             String newString = "Climate Change";
             int expected = 8;
-            assertEquals(expected, Problem31.solve(oldString, newString));
+            assertEquals(expected, Problem31.solve(oldString, newString), () -> "expected should be 8");
         }
 
         @Test
-        @DisplayName("Edit distance between 'the Swedish chef' and 'The Swedish Chef' is 2")
+        @DisplayName("Edit distance between 'the Swedish chef' and 'The Swedish Chef'")
         void solve3() {
             String oldString = "the Swedish chef";
             String newString = "The Swedish Chef";
             int expected = 2;
-            assertEquals(expected, Problem31.solve(oldString, newString));
+            assertEquals(expected, Problem31.solve(oldString, newString), () -> "expected should be 2");
         }
 
         @Test
-        @DisplayName("Edit distance between 'the Swedish chef' and 'The Swedish Chef and the lobster!' is 19")
+        @DisplayName("Edit distance between 'the Swedish chef' and 'The Swedish Chef and the lobster!'")
         void solve4() {
             String oldString = "the Swedish chef";
             String newString = "The Swedish Chef and the lobster!";
             int expected = 19;
-            assertEquals(expected, Problem31.solve(oldString, newString));
+            assertEquals(expected, Problem31.solve(oldString, newString), () -> "expected should be 19");
         }
     }
 
